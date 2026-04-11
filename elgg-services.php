309c8f1cc31aaaa56edc34942adaeb2484ff9bed
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-04-11 09:03:43 +0200

    fix(4.x): DI\object() → DI\create() in elgg-services.php

--- elgg-services.php
+++ elgg-services.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-	'email.transport' => \DI\object(\hypeJunction\Notifications\EmailTransport::class)
+	'email.transport' => \DI\create(\hypeJunction\Notifications\EmailTransport::class)
 		->constructor(\DI\get('config'), \DI\get('hooks')),
 
-	'db.notifications' => \DI\object(\hypeJunction\Notifications\SiteNotificationsTable::class)
+	'db.notifications' => \DI\create(\hypeJunction\Notifications\SiteNotificationsTable::class)
 		->constructor(\DI\get('db')),
 
-	'db.digest' => \DI\object(\hypeJunction\Notifications\DigestTable::class)
+	'db.digest' => \DI\create(\hypeJunction\Notifications\DigestTable::class)
 		->constructor(\DI\get('db')),
 
-	'notifications.site' => \DI\object(\hypeJunction\Notifications\SiteNotificationsService::class)
+	'notifications.site' => \DI\create(\hypeJunction\Notifications\SiteNotificationsService::class)
 		->constructor(\DI\get('db.notifications')),
 
-	'notifications.digest' => \DI\object(\hypeJunction\Notifications\DigestService::class)
+	'notifications.digest' => \DI\create(\hypeJunction\Notifications\DigestService::class)
 		->constructor(\DI\get('db.digest')),
 ];
